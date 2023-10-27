d9ec1827f585fdc7158e5aec5ff26f934e79362d
Seda Radoykova <seda.radoykova@gmail.com>
2023-10-27 15:10:18 +0100

    add gif_maker util func for easier visualisations

--- model/utils.R
+++ model/utils.R
@@ -1,3 +1,5 @@
+library('magick')
+
 # hyperbolic funciton for outbreak shape 
 # sinh curve used to simulate outbreak 
 shin_curve = function(xs=xdates,
@@ -13,3 +15,25 @@ shin_curve = function(xs=xdates,
     # ys = ifelse(ys<0, 0, ys)
     return(ys)
 }
+
+
+# make gifs of saved plots 
+# gifs saved and returned by default 
+gif_maker = function(
+    vec_img_paths,  # vector of full paths to desired images in correct order
+    file_name = 'plot_gif', # how to name the gif
+    dest_dir = getwd(),     # directory to save gif in 
+    .fps=2) {               # frames per second (multiple of 100)
+    # read all images into a list  
+    img_list <- lapply(vec_img_paths, image_read)
+    # join images 
+    img_joined <- image_join(img_list)
+    # animate at 2 frames per second by default
+    img_animated <- image_animate(img_joined, fps = .fps)
+    ## save to disk
+    img_path = paste0(dest_dir, '/', file_name, '.gif')
+    image_write(image = img_animated, path = img_path)
+    return(img_animated)
+}
+
+
