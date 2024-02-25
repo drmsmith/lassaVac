4b9558b0880223d69e55403975cd7e849d468ddc
Seda Radoykova <seda.radoykova@gmail.com>
2024-02-25 00:20:24 +0000

    baseline zika spread + 100/160 day histograms; sort bar plots by freq

--- visualisations/CHIK-X_vis.R
+++ visualisations/CHIK-X_vis.R
@@ -9,6 +9,7 @@ library("countrycode")
 library("ggtextures")
 library("ggpattern")
 library("grid")
+library("ggh4x")
 library("magick", quietly = T, warn.conflicts = F)
 
 conflicts_prefer(
@@ -19,7 +20,7 @@ conflicts_prefer(
 
 
 source('visualisations/utils_vis.R')
-load("figs/map_ws.RData")
+# load("figs/map_ws.RData")
 
 # CHIK-X simulation results and summaries 
 # df_all_sims_long <- read.csv("res/import_model_sim_res_x100.csv")
@@ -28,6 +29,7 @@ load("figs/map_ws.RData")
 
 main_dir <- 'res/baseline_inc_0.7_noise_0.1'
 
+df_all_sims_long <- readRDS(file.path(main_dir, "sim_res_long.RDS"))
 df_results_summary <- readRDS(file.path(main_dir, 'sim_summary.RDS'))
 df_full_summary <- readRDS(file.path(main_dir, 'sim_full_summary.RDS'))
 
@@ -50,6 +52,58 @@ df_burden$annual_incidence <- df_burden$annual_incidence*0.7
 world <- get_worldmap() # takes a minute and throws warnings 
 
 
+#### outbreak progression 
+
+df_full_summary %>% group_by(simulation) %>% count %>% arrange(n) %>% print(n=100)
+
+outbrk_data <- df_all_sims_long %>% filter(simulation==88) %>% arrange(timing)
+outbrk_data$country <- factor(outbrk_data$country, levels=unique(outbrk_data$country))
+
+df_timing = outbrk_data[,c('timing', 'country')] %>% unique
+df_timing$timing = df_timing$timing/365.25
+
+colnames(outbrk_data)
+head(outbrk_data)
+cepi_prim_cols <- c(
+    "#547dbf", "#ffa500", "#db4437", "#9d0f55", "#682860", "#0080A0", "#F9DF79"
+)
+nclrs <- length(unique(outbrk_data$country))
+v_cols <- sample(colorRampPalette(cepi_prim_cols)(nclrs), nclrs)
+
+# plot daily cases 
+outbreak_plts <- ggplot(outbrk_data, aes(x=time_years, y=daily_infections_sim)) +
+    facet_wrap(vars(country), scales = "free_y", ncol = 4) + # country or code
+    geom_point(aes(col = country), size = 0.3) +
+    geom_line(aes(col = country), linewidth=0.5) +
+    geom_vline(
+        data=df_timing, aes(xintercept = timing), 
+        color="gainsboro", linetype='dashed', lwd=0.8) + 
+    scale_color_manual(values = v_cols) +
+    theme_light() +
+    theme(legend.position = "none") + 
+    labs(x='Time (years)', y='Daily cases') + 
+    coord_cartesian(expand=FALSE)
+    
+# get all facet ylims 
+df_ylims <- map(ggplot_build(outbreak_plts)$layout$panel_params, ~data.frame(ymin=0, ymax=.x$y.range[2])) %>% bind_rows
+# create new scales for positions 
+position_scales <- pmap(df_ylims, function(ymin, ymax) {
+    return(scale_y_continuous(labels = comma, limits = c(ymin, ymax*1.1)))
+})
+# use the ggh4x package func to adjust all facet wrap scales 
+outbreak_plts <- outbreak_plts + facetted_pos_scales(y = position_scales)
+
+
+
+ggsave(outbreak_plts,
+    filename = 'figs/single_sim_outbreak_data.png', dpi=330, bg='transparent', 
+    width = 10, height = 6, units = 'in'
+)
+ggsave(outbreak_plts,
+    filename = 'figs/single_sim_outbreak_data.svg', dpi=330, bg='transparent', 
+    width = 10, height = 6, units = 'in'
+)
+
 
 #### plotting ideas 
 ### average over simulations 
@@ -64,6 +118,152 @@ world <- get_worldmap() # takes a minute and throws warnings
 
 # spillover and pop size from burden 
 # country in % sim 
+
+spread_cumul_timing_100 <- df_full_summary %>%
+    filter(outbreak_start_day <= 100) %>%
+    group_by(simulation) %>%
+    count() %>% 
+    rename(cumul_nspread = n) %>%
+    mutate(
+        simulation = factor(simulation), 
+        day = 'day 100'
+        )
+
+spread_cumul_timing_160 <- df_full_summary %>%
+    filter(outbreak_start_day <= 160) %>%
+    group_by(simulation) %>%
+    count() %>% 
+    rename(cumul_nspread = n) %>%
+    mutate(
+        simulation = factor(simulation), 
+        day = 'day 160'
+        )
+
+spread_cumul_timing_hists <- bind_rows(spread_cumul_timing_100, spread_cumul_timing_160)
+
+get_plot_limits <- function(plot) {
+    gb = ggplot_build(plot)
+    xmin = gb$layout$panel_params[[1]]$x.range[1]
+    xmax = gb$layout$panel_params[[1]]$x.range[2]
+    ymin = gb$layout$panel_params[[1]]$y.range[1]
+    ymax = gb$layout$panel_params[[1]]$y.range[2]
+    list(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax)
+}
+
+
+incid_scen_n_by_day <- ggplot(
+    spread_cumul_timing_hists,
+    aes(x = cumul_nspread)
+    ) +
+    facet_wrap(~day, ncol=2) + 
+    geom_histogram(aes(fill=day), binwidth=1, boundary = 0.5) +
+    scale_fill_manual(
+        values=c("#007492", "#e89600")) + # name='Number of\ncountries\naffected by' 
+    labs(
+        x = "Number of countries experiencing outbreaks", 
+        y = "Count"
+    ) +
+    guides(fill='none') + 
+    theme_light(base_size = 14) +
+    coord_cartesian(expand=FALSE) +  
+    scale_x_continuous(breaks=pretty_breaks(10)) + 
+    xlim(0,7) 
+    
+incid_scen_n_by_day <- incid_scen_n_by_day + ylim(0, get_plot_limits(incid_scen_n_by_day)$ymax * 1.05)
+
+ggsave(incid_scen_n_by_day,
+    filename=file.path('figs', 'hists_100_160_baseline.png'), dpi=330, 
+    width=3000, height=2000, units='px')
+
+ggsave(incid_scen_n_by_day,
+    filename=file.path('figs', 'hists_100_160_baseline.svg'), dpi=330, 
+    width=3000, height=2000, units='px')
+
+
+
+
+spread_cumul_timing <- df_full_summary %>%
+    group_by(simulation) %>%
+    add_count(code) %>%
+    mutate(
+        cumul_nspread = cumsum(n),
+        simulation = factor(simulation)
+    )
+
+
+wider_cumul_spread <- spread_cumul_timing %>% 
+    group_by(simulation) %>%
+    make_wider_cumul_spread() 
+
+# subset and summarise cdf for test 
+sum_cumul_spread <- wider_cumul_spread %>% 
+        mean_median_trajectory()
+        
+
+ncols = 1
+
+# prepare zika baseline 
+df_zika_cumul <- make_df_zika_cumul()
+
+rate_tune_plot <- ggplot(
+    spread_cumul_timing,
+    aes(x = outbreak_start_day / 365, y = cumul_nspread)
+    ) +
+    geom_line(
+        aes(group = simulation), color = "#8ca8d4", # "#bd5e8d", # "darkgray", 
+        linewidth = 0.8, alpha = 0.45
+        ) +
+    geom_line(
+        data = df_zika_cumul, aes(x = date_norm, y = cumul_nspread),
+        linewidth = 1, color = "#5f2457",# "#5f2457" # "black"
+        alpha=0.9
+    ) + 
+    geom_line(
+        data = sum_cumul_spread, aes(x = days/365, y = mean_nspread),
+        linewidth = 1, color = "#2e4569",# "#5f2457" # "black"
+        linetype='dotted', alpha=0.9
+    ) +
+    geom_line(
+        data = sum_cumul_spread, aes(x = days/365, y = median_nspread),
+        linewidth = 1, color = "#2e4569",# "#5f2457" # "black"
+        linetype='dashed', alpha=0.9
+    ) +
+    guides(color = "none") +
+    xlim(0, 2.1) +
+    coord_cartesian(expand=FALSE) + 
+    labs(
+        x = "Time (years)", 
+        y = "Number of countries\nexperiencing outbreaks"
+    ) +
+    theme_light(base_size = 16)
+    rate_tune_plot
+    
+rate_tune_plot <- rate_tune_plot + ylim(0, get_plot_limits(rate_tune_plot)$ymax * 1.05)
+rate_tune_plot
+
+
+ggsave(rate_tune_plot,
+    filename=file.path('figs', 'baseline_zika_spread.png'), dpi=330, 
+    width=2000, height=1500, units='px')
+
+ggsave(rate_tune_plot,
+    filename=file.path('figs', 'baseline_zika_spread.svg'), dpi=330, 
+    width=2000, height=1500, units='px')
+
+
+
+baseline_plots = plot_grid(rate_tune_plot,incid_scen_n_by_day, labels = "AUTO", ncol=2) 
+baseline_plots
+
+ggsave(baseline_plots,
+    filename=file.path('figs', 'zika_spread_and_histograms.png'), dpi=330, 
+    width=3300, height=1700, units='px')
+
+ggsave(baseline_plots,
+    filename=file.path('figs', 'zika_spread_and_histograms.svg'), dpi=330, 
+    width=3300, height=1700, units='px')
+
+
 
 
 # this gives how frequently a given country appears in the simulations 
@@ -91,6 +291,7 @@ df_sum_stats <- df_full_summary %>%
     summarise(
         # pop_size = unique(pop_size),
         # total cumulative mean, median, IQR 
+        percentage_appearance = n(),
         mean_cumul_infections = mean(total_infections_all_years),
         median_cumul_infections = median(total_infections_all_years),
         q1_cumul_infections = quantile(total_infections_all_years, 0.25),
@@ -115,8 +316,7 @@ df_sum_stats <- df_full_summary %>%
     ) %>%      
     left_join(  # join with p_spillover, percent_sim, and ppp_size
         percent_pop_size_spillover[,c('code', 'pop_size', 'p_spillover', 'percent_sims')], by = join_by(code == code)
-        ) %>%
-    ungroup 
+        ) %>% ungroup 
 
 # add 0 for v_shape_pop_burden_mobil
 # countries that are in simulations but do not have outbreaks 
@@ -162,6 +362,7 @@ cepi_prim_cols <- c(
     "#547dbf", "#ffa500", "#db4437", "#9d0f55", "#682860", "#0080A0", "#F9DF79"
 )
 
+
 all_metrics <- c(
     "median_cumul_infections",
     "median_cumul_infections_per100k"
@@ -171,6 +372,31 @@ all_metrics <- c(
 
 
 region_codes <- c("AFR", "AMR", "EMR", "EUR", "SEAR", "WPR")
+
+names(cepi_prim_cols) <- region_codes
+
+
+barplotdir <- "figs/barplots"
+walk(all_metrics, function(.curr_metric) {
+    walk(region_codes, function(.reg_code) {
+        dest_dir = barplotdir
+        # dest_dir <- paste0(dest_dir, .reg_code, collapse = "")
+        if (!dir.exists(dest_dir)) dir.create(dest_dir)
+        # run plotting funcs
+        fill_col <- cepi_prim_cols[.reg_code]
+        gg_metrics_barplot_region_col_sort(
+            .data = df_sum_stats, 
+            .cols_for_scheme = cepi_prim_cols, 
+            .region_code = .reg_code, 
+            .fill_col = fill_col, 
+            .metric = .curr_metric,
+            .dest_dir = dest_dir
+        )
+    })
+}, .progress = T)
+
+
+
 
 
 barplotdir <- "figs/barplots"
@@ -189,7 +415,6 @@ walk(all_metrics, function(.curr_metric) {
         )
     })
 }, .progress = T)
-
 
 
 # df_by_region <- df_full_summary %>% group_by(region_code, region_name) %>% 
