1942ae04ed511fbb7103339c6d86cff93c12f484
Seda Radoykova <seda.radoykova@gmail.com>
2024-02-25 20:16:05 +0000

    WIP: outbreak progression arrows gifs

--- visualisations/vis_map_spread_arrows.R
+++ visualisations/vis_map_spread_arrows.R
@@ -17,21 +17,31 @@ conflicts_prefer(
     .quiet = T
 )
 
-
 source('visualisations/utils_vis.R')
+source("visualisations/utils_ks_test.R")
+source("visualisations/utils_post_proc.R")
 
 main_dir <- 'res/baseline_inc_0.7_noise_0.1'
+incidence_factor <- 0.7
+dest_dir <- file.path('figs','baseline_zika_and_hists')
 
+df_all_sims_long <- readRDS(file.path(main_dir, "sim_res_long.RDS"))
 df_results_summary <- readRDS(file.path(main_dir, 'sim_summary.RDS'))
 df_full_summary <- readRDS(file.path(main_dir, 'sim_full_summary.RDS'))
 
 # helper files
 df_burden = read.csv('data/2019ppp_df_suit_means_pop_wght_pop_size_who_p_spillover.csv') 
+df_burden$annual_incidence <- df_burden$annual_incidence*incidence_factor
 
-df_burden$annual_incidence <- df_burden$annual_incidence*0.7
+# get world map for plotting 
+world <- get_worldmap() # takes a minute and throws warnings 
 
-# load world map for plotting 
-world <- get_worldmap_nocode() # takes a minute and throws warnings 
+
+
+
+######################
+# file preprocessing # 
+######################
 
 # this gives how frequently a given country appears in the simulations 
 # p spillover and pop_size 
@@ -52,12 +62,10 @@ df_sum_stats <- df_full_summary %>%
     mutate(
         country = factor(country, levels=unique(country)), 
         per_100k = 1e5*total_infections_all_years/pop_size
-        # per_100k_2yrs = 1e5*years_1_2/pop_size
         ) %>%
     group_by(country, code, region_name, region_code) %>%
     summarise(
-        # pop_size = unique(pop_size),
-        # total cumulative mean, median, IQR 
+        percentage_appearance = n(),
         mean_cumul_infections = mean(total_infections_all_years),
         median_cumul_infections = median(total_infections_all_years),
         q1_cumul_infections = quantile(total_infections_all_years, 0.25),
@@ -70,8 +78,7 @@ df_sum_stats <- df_full_summary %>%
     ) %>%      
     left_join(  # join with p_spillover, percent_sim, and ppp_size
         percent_pop_size_spillover[,c('code', 'pop_size', 'p_spillover', 'percent_sims')], by = join_by(code == code)
-        ) %>%
-    ungroup 
+        ) %>% ungroup 
 
 # add 0 for v_shape_pop_burden_mobil
 # countries that are in simulations but do not have outbreaks 
@@ -90,15 +97,6 @@ df_sum_stats_zeros_NAs = bind_rows(df_sum_stats, df_zeros_NAs)
 # optionally set 0 for NAs if needed
 df_sum_stats_zeros_NAs[is.na(df_sum_stats_zeros_NAs)] = 0 
 ls_sum_stats <- df_sum_stats_zeros_NAs %>% group_by(region_code) %>% group_split()
-
-
-# how to format colour scale legends??
-colnames(df_sum_stats_zeros_NAs)
-apply(df_sum_stats_zeros_NAs, 2, max) %>% 
-    as.numeric %>% 
-    .[!(is.na(.))] %>%
-    format(digits=3, scientific=T)
-
 
 
 
@@ -453,3 +451,90 @@ walk2(n_spread$simulation, n_spread$n_countries, function(.simid, .nspread) {
     )
 }, .progress=T)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#############################
+# OUTBREAK PROGRESSION GIFS # 
+#############################
+
+
+# check n countries spread 
+n_spread <- data.frame(unlist(table(df_results_summary$simulation)))
+colnames(n_spread) <- c("simulation", "n_countries")
+n_spread
+
+
+dest_dir <- file.path("figs", "outbreak_progression")
+if (!dir.exists(dest_dir)) dir.create(dest_dir)
+
+# filter simulation data 
+sim_no <- 94
+df_sim = df_full_summary %>% filter(simulation==sim_no) %>% arrange(outbreak_start_day) %>%
+    mutate(
+        per_100k = 1e5*total_infections_all_years/pop_size
+    )
+
+
+# full map for plotting 
+wrld_joined_full <- left_join(
+    world, df_sim,
+    by = join_by(adm0_a3 == code)
+)
+
+# add NAs 
+wrld_joined_nas <- left_join(
+    world, df_burden,
+    by = join_by(adm0_a3 == code)
+) %>% filter(!complete.cases(pop_size))
+
+
+# loop over every stage of the map and add 
+outbr_map <- gg_map_outbreak_progress(
+    .wrld_joined_full=wrld_joined_full,
+    .wrld_joined_nas=wrld_joined_nas, 
+    .metric='per_100k', # total_infections_all_years
+    .filter_codes=ccodes_filter,
+    .grob=grob,
+    .start_day=start_day,
+    .dest_dir = dest_dir
+    )
+
+ 
+
+gif_maker = function(
+    vec_img_paths,  # vector of full paths to desired images in correct order
+    file_name = 'plot_gif', # how to name the gif
+    dest_dir = getwd(),     # directory to save gif in 
+    .fps=2) {               # frames per second (multiple of 100)
+    # read all images into a list  
+    img_list <- lapply(vec_img_paths, image_read)
+    # join images 
+    img_joined <- image_join(img_list)
+    # animate at 2 frames per second by default
+    img_animated <- image_animate(img_joined, fps = .fps)
+    ## save to disk
+    img_path = paste0(dest_dir, '/', file_name, '.gif')
+    image_write(image = img_animated, path = img_path)
+    gc()
+    return(img_animated)
+}
+
+
+source('model/utils.R')
+all_filepaths = list.files(dest_dir, full.names = T)
+gif_maker(all_filepaths, "outbreak_progression", dest_dir, .fps = 1)
